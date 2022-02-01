a48c46a12c84678434eedcc077508fff643c08f3
Pekkakost <pekkaj.kostiainen@gmail.com>
2022-02-01 11:17:14 +0200

    commit kaski

--- omistuskirjoitus.py
+++ omistuskirjoitus.py
@@ -3,4 +3,5 @@ mihin = input("mihin kirjoitetaan:")
 
 with open(mihin, "w") as tiedosto:
     print("jooseppi laulaa")
+    print("tama on muutos")
     print(f"Hei {nimi}, toivomme viihtyisiä hetkiä python-kurssimateriaalin parissa! Terveisin mooc.fi-tiimi")
